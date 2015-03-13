3eea244d1d14c593c6436ae0f24ebccb0dcf2144
CUI Hao <cuihao.leo@gmail.com>
2015-03-13 20:00:16 +0800

    New: 计算方法作业1 的多线程版本

---
+++ jsff-hw1-mt/src/main.rs
@@ -0,0 +1,31 @@
+/* 计算方法作业1：计算 Sum(1/(k*(k+x))), k from 1 to +inf
+ * 并行计算版本 */
+
+#![feature(core)]
+use std::iter::AdditiveIterator;
+use std::f64;
+use std::sync::mpsc;
+use std::thread;
+
+fn psi (x: f64) -> f64 {
+    (1..1000000).map(|ki| {
+        let k = ki as f64;
+        1.0/(k*(k+x))
+    }).sum()
+}
+
+fn main() {
+    let xlist = [0.0, 0.5, 1.0, f64::consts::SQRT2, 10.0, 100.0, 300.0];
+    let (tx, rx) = mpsc::channel();
+
+    for &x in xlist.iter() {
+        let tx = tx.clone();
+        thread::spawn(move || tx.send([x, psi(x)]).unwrap());
+    }
+
+    for _ in 0..xlist.len() {
+        let [x, psi] = rx.recv().ok().expect(":)");
+        println!("{:6.2} , {:16.12e}", x, psi);
+    }
+}
+
