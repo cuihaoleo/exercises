d1324b07f667bc663a84d9985cdb17545bcfb240
CUI Hao <cuihao.leo@gmail.com>
2015-08-27 01:18:38 +0800

    jsff-hw1-mt works

--- jsff-hw1-mt/src/main.rs
+++ jsff-hw1-mt/src/main.rs
@@ -1,17 +1,15 @@
 /* 计算方法作业1：计算 Sum(1/(k*(k+x))), k from 1 to +inf
  * 并行计算版本 */
 
-#![feature(core)]
-#![feature(slice_patterns)]
 use std::f64;
 use std::sync::mpsc;
 use std::thread;
 
-fn psi (x: f64) -> f64 {
+fn psi(x: f64) -> f64 {
     (1..1000000).map(|ki| {
         let k = ki as f64;
         1.0/(k*(k+x))
-    }).sum()
+    }).fold(0.0, |acc, elem| acc+elem)
 }
 
 fn main() {
@@ -20,11 +18,11 @@ fn main() {
 
     for &x in xlist.iter() {
         let tx = tx.clone();
-        thread::spawn(move || tx.send([x, psi(x)]).unwrap());
+        thread::spawn(move || tx.send((x, psi(x))).unwrap());
     }
 
     for _ in 0..xlist.len() {
-        let [x, psi] = rx.recv().unwrap();
+        let (x, psi) = rx.recv().unwrap();
         println!("{:6.2} , {:16.12e}", x, psi);
     }
 }
