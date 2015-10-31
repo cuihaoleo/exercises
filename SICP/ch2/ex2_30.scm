1dfeecd2dd33b089559e8e61db0d644a885bac16
CUI Hao <cuihao.leo@gmail.com>
2015-10-31 17:28:50 +0900

    SICP update

---
+++ SICP/ch2/ex2_30.scm
@@ -0,0 +1,8 @@
+(define (square-tree tree)
+ (cond ((null? tree) '())
+       ((not (pair? tree)) (* tree tree))
+       (else (cons (square-tree (car tree))
+                   (square-tree (cdr tree))))))
+
+(display (square-tree
+          (list 1 (list 2 (list 3 4) 5))))
