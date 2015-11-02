6e17417384a871dff7892215e4dcf9c298c82fd2
CUI Hao <cuihao.leo@gmail.com>
2015-11-02 14:27:25 +0900

    LeetCode: Find the Duplicate Number

---
+++ LeetCode/algorithms/find-the-duplicate-number/solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int findDuplicate(int[] nums) {
+        int slow = nums[0], fast = nums[nums[0]];
+
+        while (fast != slow) {
+            fast = nums[nums[fast]];
+            slow = nums[slow];
+        }
+        
+        fast = 0;
+        while (fast != slow) {
+            fast = nums[fast];
+            slow = nums[slow];
+        }
+
+        return slow;
+    }
+}
