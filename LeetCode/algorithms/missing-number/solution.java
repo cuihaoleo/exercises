19aecba2b3341ec1dbd4f13011b7c8b1b1ec22dc
CUI Hao <cuihao.leo@gmail.com>
2015-11-02 15:26:37 +0900

    LeetCode: Missing Number

---
+++ LeetCode/algorithms/missing-number/solution.java
@@ -0,0 +1,8 @@
+public class Solution {
+    public int missingNumber(int[] nums) {
+        int total = nums.length * (nums.length+1) / 2;
+        for (int i: nums)
+			total -= i;
+        return total;
+    }
+}
